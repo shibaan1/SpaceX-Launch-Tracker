018249a8c1ab0aada75281ff5931b35c1a47dfaf
Shibaan Askari <askarishibaan1@gmail.com>
2026-08-23 20:22:57 +0530

    feat: add mock data JSON and integrate it into Home

--- src/main.jsx
+++ src/main.jsx
@@ -47,7 +47,7 @@ const router = createBrowserRouter(
 )
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ 
     <RouterProvider router={router} />
-  </StrictMode>,
+  
 )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,13 @@
 import React from 'react'
-import useFetch from '../hooks/useFetch'
+import launches from '../data/launches.json'
 
 const Home = () => {
-  const {data , loading , error} = useFetch('https://api.spacexdata.com/v3/launches')
 
-  console.log(data)
-
-  if (loading) return <p>loading....</p>
-  if(error) return <p>Erro:{error}</p>
+  console.log(launches.length)
   return (
     <div>
       Home
-    
-      
+
     </div>
   )
 }
