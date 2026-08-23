040c7b25ea832dc300dfe65e9876fb51286db453
Shibaan Askari <askarishibaan1@gmail.com>
2026-08-23 22:17:39 +0530

    feat: add loading spinner and error state to Home

--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const LoadingSpinner = () => {
   return (
     <div>
-      loading spinner
+      loading......
     </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import launches from '../data/launches.json'
 import LaunchCard from '../components/LaunchCard.jsx'
+import LoadingSpinner from '../components/LoadingSpinner.jsx'
 
 const Home = () => {
+  const error = null
+
+  if (launches.length === 0) { return <LoadingSpinner /> }
+  if (error) { return <p>error:{error}</p> }
 
   return (
     <div>
@@ -13,7 +18,6 @@ const Home = () => {
           </li>
         )}
       </ul>
-
     </div>
   )
 }
