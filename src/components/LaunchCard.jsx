ca7e74fec7691c537dabd7ac55596548d5b3b509
Shibaan Askari <askarishibaan1@gmail.com>
2026-08-23 22:09:35 +0530

    feat: add launch list with LaunchCard component

--- src/components/LaunchCard.jsx
+++ src/components/LaunchCard.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 
-const LaunchCard = () => {
+
+const LaunchCard = ({ launch }) => {
+
+  const result = () => {
+    if (launch.success) {
+      return 'successfuly launched'
+
+    }
+    else {
+      return 'failed to launch'
+    }
+  }
+
   return (
     <div>
-      launch card
+      <p>mission name: {launch.name}</p>
+      <p>flight number: {launch.flight_number}</p>
+      <p>launch date: {launch.date_utc}</p>
+      <p>success: {result()}</p>
+      <img src={launch.links.patch.small} alt="no image" />
     </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import launches from '../data/launches.json'
+import LaunchCard from '../components/LaunchCard.jsx'
 
 const Home = () => {
 
-  console.log(launches.length)
   return (
     <div>
       Home
+      <ul>
+        {launches.map((launch) =>
+          <li key={launch.id}> <LaunchCard launch={launch} />
+          </li>
+        )}
+      </ul>
 
     </div>
   )
