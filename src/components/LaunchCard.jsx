19dc6263647163b9038c75163f1858b8b748ea30
Shibaan Askari <askarishibaan1@gmail.com>
2026-08-26 00:33:27 +0530

    feat: add launch details page and optimize LaunchCard with React.memo

--- src/components/LaunchCard.jsx
+++ src/components/LaunchCard.jsx
@@ -1,27 +1,34 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 
-const LaunchCard = ({ launch }) => {
+const LaunchCard = React.memo(
+  ({ launch }) => {
 
-  const result = () => {
-    if (launch.success) {
-      return 'successfuly launched'
+    const result = () => {
+      if (launch.success) {
+        return 'successfuly launched'
 
+      }
+      else {
+        return 'failed to launch'
+      }
     }
-    else {
-      return 'failed to launch'
-    }
+
+    return (
+      <div>
+        <Link to={`/launch/${launch.id}`}>
+
+          <p>mission name: {launch.name}</p>
+          <p>flight number: {launch.flight_number}</p>
+          <p>launch date: {launch.date_utc}</p>
+          <p>success: {result()}</p>
+          <img src={launch.links.patch.small} alt="no image" />
+
+        </Link>
+      </div>
+    )
   }
 
-  return (
-    <div>
-      <p>mission name: {launch.name}</p>
-      <p>flight number: {launch.flight_number}</p>
-      <p>launch date: {launch.date_utc}</p>
-      <p>success: {result()}</p>
-      <img src={launch.links.patch.small} alt="no image" />
-    </div>
-  )
-}
-
+)
 export default LaunchCard
--- src/pages/LaunchDetail.jsx
+++ src/pages/LaunchDetail.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
+import { useNavigate, useParams } from 'react-router'
+import launches from '../data/launches.json'
 
 const LaunchDetail = () => {
+
+  const { id } = useParams()
+  const navigate = useNavigate()
+
+  const findResult = launches.find((launch) => launch.id === id)
+
+  const statusResult = () => {
+    if (findResult.success) {
+      return 'successfuly launched'
+    }
+
+    else {
+      return 'failed to launch'
+    }
+  }
+
+  const handleClick = () => {
+    navigate('/')
+  }
+
   return (
     <div>
+      <button onClick={handleClick}>go back to home</button>
       launch details
+      <p>mission name: {findResult.name}</p>
+      <p>Flight number: {findResult.flight_number}</p>
+      <p>Launch date: {findResult.date_utc}</p>
+      <p>Status: {statusResult()}</p>
+      <img src={findResult.links.patch.small} alt="" />
+      <p>details: {findResult.details}</p>
+      <a href={findResult.links.webcast}>vedio Link</a>
     </div>
   )
 }
