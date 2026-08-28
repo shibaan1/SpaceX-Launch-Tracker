06a11cd09162a785def3228d07a1b45527299478
Shibaan Askari <askarishibaan1@gmail.com>
2026-08-28 16:54:54 +0530

    feat: add launch statistics and most used rocket calculations

--- src/pages/Statistics.jsx
+++ src/pages/Statistics.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useMemo } from 'react'
+import launches from '../data/launches.json'
 
 const Statistics = () => {
+
+  const successRate = useMemo(() => {
+
+    const successful = launches.filter((launch) => launch.success === true)
+
+    return (successful.length / launches.length) * 100
+
+
+  }, [])
+
+  const mostUsed = useMemo(() => {
+
+    return launches.reduce((acc, launch) => {
+
+      acc[launch.rocket] = (acc[launch.rocket] || 0) + 1
+      return acc
+
+    }, {})
+  }, [])
+
+  const topRocket = useMemo(() => {
+    return Object.keys(mostUsed).reduce((acc, curr) => {
+
+      return mostUsed[curr] > mostUsed[acc] ? curr : acc
+
+    })
+  },[ mostUsed])
+
+
   return (
     <div>
       statistics
+      <p>total launches: {launches.length}</p>
+      <p>success rate: {successRate}%</p>
+      <p>most used rocket : {topRocket}</p>
     </div>
   )
 }
